137267b928a942aab42137a2fb7e6069c01cc3c8
jgarc172 <jgarc172@mail.ccsf.edu>
2022-07-16 17:54:24 +0000

    wc example

--- wc/main_test.go
+++ wc/main_test.go
@@ -1,39 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"strings"
 	"testing"
 )
 
+func ExampleCount() {
+	reader := strings.NewReader("Hello World")
+	fmt.Println(count(reader))
+	// Output:
+	// 2
+}
+
 // tests count(r io.Reader) int
 func TestCount(t *testing.T) {
-	for _, test := range testData() {
-		t.Run(test.name, func(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
 
-			got := count(test.input)
+			got := count(tc.input)
 
-			if got != test.want {
-				t.Errorf("got '%d' want '%d'", got, test.want)
+			if got != tc.want {
+				t.Errorf("got '%d' want '%d'", got, tc.want)
 			}
 		})
 	}
 }
 
-func newReader(s string) io.Reader {
-	return strings.NewReader(s)
-}
-
-type test struct {
+var testCases = []struct {
 	name  string
 	input io.Reader
 	want  int
-}
-
-func testData() []test {
-	return []test{
-		{"empty", newReader(""), 0},
-		{"one line", newReader("one two three\n"), 3},
-		{"lines", newReader("one two three\nfour five\n"), 5},
-	}
+}{
+	{"empty", strings.NewReader(""), 0},
+	{"one line", strings.NewReader("one two three\n"), 3},
+	{"lines", strings.NewReader("one two three\nfour five\n"), 5},
 }
